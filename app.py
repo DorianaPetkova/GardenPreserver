8e0370aa72d62fd7b389d31ed37caad4b8da7e26
Doriana <133523673+DorianaPetkova@users.noreply.github.com>
2024-05-16 21:42:02 +0300

    Search function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from flask import Flask, render_template, request
-
+import os
 
 db_config = {
     'host': 'localhost',
@@ -11,19 +11,14 @@ db_config = {
 
 app = Flask(__name__)
 
-
 def get_flower_info(name):
-    
     mydb = mysql.connector.connect(**db_config)
     cursor = mydb.cursor()
-   
-    query = "SELECT name, description FROM flowers WHERE name = %s"
+    query = "SELECT id, name, description FROM flowers WHERE name = %s"
     cursor.execute(query, (name,))
     flower = cursor.fetchone()
-
     cursor.close()
     mydb.close()
-
     return flower
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,7 +27,9 @@ def index():
         flower_name = request.form['flower_name']
         flower_info = get_flower_info(flower_name)
         if flower_info:
-            return render_template('result.html', flower_name=flower_info[0], flower_description=flower_info[1])
+            flower_id, flower_name, flower_description = flower_info
+            image_path = f"/pictures/{flower_id}.jpg"  # Assuming the image name is same as flower ID
+            return render_template('result.html', flower_name=flower_name, flower_description=flower_description, image_path=image_path)
         else:
             return render_template('result.html', error_message="Flower not found.")
     return render_template('index.html')
